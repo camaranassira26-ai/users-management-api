cac1525e526a6dbe2ab3320f26b6c4746251df4d
nassiracamara <n_camara6@stu-digital-campus.fr>
2026-05-13 14:36:01 +0200

    fix conflits index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import express from 'express';
 import morgan from "morgan";
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-<<<<<<< HEAD
-=======
 import movieRoutes from './routes/movieRoutes.js';
->>>>>>> bcf5ab1479cfc1598961efd5ab5e0c5e61c4dbdc
 import errorHandler from './middlewares/errorHandler.js';
 
 const PORT = 3000;
@@ -17,31 +14,17 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
     res.json({
-        message: 'welcome to HETIC users management API!!'
+        message: 'Welcome to HETIC users management API!!'
     });
-});
-
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-=======
-  res.json({
-    message: 'Welcome to HETIC users management!!'
-  });
 });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes);
->>>>>>> bcf5ab1479cfc1598961efd5ab5e0c5e61c4dbdc
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
-    console.log(`Server is running on https://localhost:${PORT}`)
-=======
-  console.log(`Server is running on http://localhost:${PORT}`);
->>>>>>> bcf5ab1479cfc1598961efd5ab5e0c5e61c4dbdc
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
