606647ff05725e025dbc8bc828eea967f747daec
nassiracamara <n_camara6@stu-digital-campus.fr>
2026-05-13 14:59:03 +0200

    fix userRoutes conflits

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,5 @@
 import { Router } from "express";
 import * as userController from "../controllers/userController.js";
-<<<<<<< HEAD
-
-const router = Router();
-
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
-=======
 import authenticate from "../middlewares/authenticate.js";
 
 const router = Router();
@@ -19,6 +9,5 @@ router.get("/:id", authenticate, userController.getUserById);
 router.post("/", authenticate, userController.createUser);
 router.put("/:id", authenticate, userController.updateUser);
 router.delete("/:id", authenticate, userController.deleteUser);
->>>>>>> bcf5ab1479cfc1598961efd5ab5e0c5e61c4dbdc
 
 export default router;
